62ae88d1a83dd0d8345bdbf58b49b0be1f84fbb5
xXStromRiderXx <stromrider@fragged.zone>
2026-03-27 00:55:50 +0100

    init with jazz2

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          jazz2 = pkgs.callPackage ./games/jazz2 { };
+        }
+      );
+    };
+}
