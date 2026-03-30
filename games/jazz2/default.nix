7d4e6a0679c912261ecb7ecc80e36e03ba58b9f3
xXStromRiderXx <stromrider@fragged.zone>
2026-03-31 01:19:41 +0200

    jazz2: convert to mkGame native with overlayfs and gamescope

--- games/jazz2/default.nix
+++ games/jazz2/default.nix
@@ -2,59 +2,53 @@
   fetchurl,
   jazz2,
   lib,
-  runCommandLocal,
-  stdenvNoCC,
+  pkgs,
   unzip,
-  writeShellScript,
 }:
 
 let
+  mkGame = import ../../lib/mk-game.nix { inherit lib pkgs; };
+
   jazz2-mp = jazz2.overrideAttrs (old: {
     cmakeFlags = old.cmakeFlags ++ [
       (lib.cmakeBool "WITH_MULTIPLAYER" true)
     ];
   });
+in
+mkGame {
+  name = "jazz2";
 
-  gameData = fetchurl {
+  src = fetchurl {
     url = "https://archive.org/download/jazz-jackrabbit-2-1.24-the-secret-files-plus/Jazz%20Jackrabbit%202%201.24%20The%20Secret%20Files%20Plus.zip";
     hash = "sha256-ceeXiGUy5QDq1HQPo5gpyQe2odAK1F5frofBXegeXi8=";
     name = "jazz-jackrabbit-2-tsf.zip";
   };
 
-  sourceData =
-    runCommandLocal "jazz2-source-data"
-      {
-        nativeBuildInputs = [ unzip ];
-      }
-      ''
-        mkdir -p "$out/Jazz² Resurrection/Source"
-        unzip -j -o ${gameData} -d "$out/Jazz² Resurrection/Source/"
-      '';
+  nativeBuildInputs = [ unzip ];
 
-  wrapper = writeShellScript "jazz2" ''
-    export XDG_DATA_HOME="''${HOME:-.}/.strom/jazz2"
-    datadir="$XDG_DATA_HOME/Jazz² Resurrection"
-    if [ ! -e "$datadir/Source/Anims.j2a" ]; then
-      mkdir -p "$datadir"
-      rm -rf "$datadir/Source"
-      ln -sfn "${sourceData}/Jazz² Resurrection/Source" "$datadir/Source"
-    fi
-    exec ${lib.getExe jazz2-mp} "$@"
+  buildScript = ''
+    mkdir -p "$out"
+    unzip -j -o $src -d "$out/"
   '';
-in
 
-stdenvNoCC.mkDerivation {
-  pname = "jazz2-with-gamedata";
-  inherit (jazz2) version;
+  runtime = "native";
 
-  dontUnpack = true;
+  runScript = ''
+    # jazz2 looks for data at XDG_DATA_HOME/Jazz² Resurrection/Source/
+    export XDG_DATA_HOME="$GAMEDIR/.xdg-data"
+    datadir="$XDG_DATA_HOME/Jazz² Resurrection"
+    mkdir -p "$XDG_DATA_HOME"
+    ln -sfn "$GAMEDIR" "$datadir/Source" 2>/dev/null || {
+      mkdir -p "$datadir"
+      ln -sfn "$GAMEDIR" "$datadir/Source"
+    }
 
-  installPhase = ''
-    mkdir -p $out/bin
-    ln -s ${wrapper} $out/bin/jazz2
+    exec gamescope -W 1920 -H 1080 -w 1920 -h 1080 --expose-wayland -- \
+      ${lib.getExe jazz2-mp}
   '';
 
   meta = jazz2.meta // {
     description = "Jazz Jackrabbit 2 (Jazz² Resurrection with game data and multiplayer)";
+    mainProgram = "jazz2";
   };
 }
