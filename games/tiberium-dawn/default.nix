9507704c9a11f316c584290681a9393fb99fb6d3
xXStromRiderXx <stromrider@fragged.zone>
2026-03-31 02:31:29 +0200

    tiberium-dawn: add Command & Conquer: Tiberian Dawn (Vanilla Conquer, native)

---
+++ games/tiberium-dawn/default.nix
@@ -0,0 +1,81 @@
+{
+  lib,
+  pkgs,
+  fetchurl,
+  p7zip,
+}:
+
+let
+  mkGame = import ../../lib/mk-game.nix { inherit lib pkgs; };
+
+  vanillatd = pkgs.vanillatd;
+
+  gdiIso = fetchurl {
+    url = "https://archive.org/download/cnc-dos-eng-v-1.22/C%26C%20DOS%20ENG%20v1.22%20Disk%201%20-%20GDI.iso";
+    hash = "sha256-kp12oZiKs2Z4c97LTRd1ORnIwvil1Me/3jhSWj+naUs=";
+    name = "cnc-gdi.iso";
+  };
+
+  nodIso = fetchurl {
+    url = "https://archive.org/download/cnc-dos-eng-v-1.22/C%26C%20DOS%20ENG%20v1.22%20Disk%202%20-%20Nod.iso";
+    hash = "sha256-cH/ob8JqcT/0VjrS1WObClNHbMLrZWr5d5thFALwxdA=";
+    name = "cnc-nod.iso";
+  };
+in
+mkGame {
+  name = "tiberium-dawn";
+
+  src = gdiIso;
+
+  nativeBuildInputs = [ p7zip ];
+
+  buildScript = ''
+    mkdir -p "$out"
+
+    # Extract MIX files from both discs
+    cd /tmp
+    mkdir -p gdi nod
+    cd gdi && 7z x ${gdiIso} -aoa && cd ..
+    cd nod && 7z x ${nodIso} -aoa && cd ..
+
+    # Copy all MIX files (lowercase for vanillatd)
+    for f in gdi/*.MIX; do
+      name=$(basename "$f" | tr '[:upper:]' '[:lower:]')
+      cp "$f" "$out/$name"
+    done
+
+    # Copy Nod movies (different from GDI)
+    cp nod/MOVIES.MIX "$out/movies_nod.mix"
+
+    # Copy speech from subdirectory
+    cp gdi/AUD1/SPEECH.MIX "$out/speech.mix"
+
+    # Copy transit and local from install dir
+    cp gdi/INSTALL/TRANSIT.MIX "$out/transit.mix"
+    cp gdi/INSTALL/LOCAL.MIX "$out/local.mix"
+
+    rm -rf gdi nod
+  '';
+
+  runtime = "native";
+
+  runScript = ''
+    export XDG_CONFIG_HOME="$GAMEDIR/.config"
+
+    # Write vanillatd.ini so it finds data in the overlay and saves to GAMEDIR
+    cat > "$GAMEDIR/vanillatd.ini" <<EOF
+    [Paths]
+    DataPath=$GAMEDIR
+    UserPath=$GAMEDIR
+    EOF
+
+    exec gamescope -W 1920 -H 1080 -w 1920 -h 1080 -r 60 --force-grab-cursor --expose-wayland -- \
+      ${vanillatd}/bin/vanillatd -cd"$GAMEDIR"
+  '';
+
+  meta = {
+    description = "Command & Conquer: Tiberian Dawn (Vanilla Conquer, native)";
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "tiberium-dawn";
+  };
+}
