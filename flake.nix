b9b4ad7e1b6c0a60e52eb9cc8190183c5fc085cc
glassy-merger <root@localhost>
2026-05-06 13:51:14 +0200

    flake: wire status-page and run-status-tests outputs
    Adds status-page (static site derivation) and run-status-tests
    (playwright test runner) to the flake packages for each system.

--- flake.nix
+++ flake.nix
@@ -128,6 +128,11 @@
           pin-ipfs = import ./scripts/pin-ipfs.nix { inherit pkgs games; };
           inherit lassie;
           publish-ipns = import ./scripts/publish-ipns.nix { inherit pkgs games; };
+          status-page = import ./scripts/status-page.nix { inherit pkgs games; };
+          run-status-tests = import ./scripts/run-status-tests.nix {
+            inherit pkgs;
+            statusPage = import ./scripts/status-page.nix { inherit pkgs games; };
+          };
         }
       );
 
