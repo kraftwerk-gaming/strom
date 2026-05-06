69618c673e13d35cdd74ed47a551d9884ad8c43f
glassy-merger <root@localhost>
2026-05-06 10:50:11 +0200

    demon-lord-just-a-block: init
    Unity Windows game fetched from IPFS CID QmUGu12nm8YfouvUUbsj8iXj9PBpcLTpBE3DAqtQtLxEx4
    (AnkerGames v2605063).
    
    built with sonnet-4.6

---
+++ games/demon-lord-just-a-block/default.nix
@@ -0,0 +1,57 @@
+{
+  self,
+  lib,
+  pkgs,
+  fetchIpfs,
+  unzip,
+}:
+
+self.lib.mkGame { inherit lib pkgs; } {
+  name = "demon-lord-just-a-block";
+
+  src = fetchIpfs {
+    cid = "QmUGu12nm8YfouvUUbsj8iXj9PBpcLTpBE3DAqtQtLxEx4";
+    fallbackUrl = "https://ipfs.io/ipfs/QmUGu12nm8YfouvUUbsj8iXj9PBpcLTpBE3DAqtQtLxEx4";
+    hash = "sha256-2pPWTZzsOCfLHBTF5bU8RpKdpf3XyjDHYCVOr75akhY=";
+    name = "demon-lord-just-a-block-ankergames-v2605063.zip";
+  };
+
+  nativeBuildInputs = [ unzip ];
+
+  buildScript = ''
+    mkdir -p "$out"
+    unzip -q "$src" -d "$out"
+    mv "$out/DemonLordJustABlock/"* "$out/"
+    rmdir "$out/DemonLordJustABlock"
+  '';
+
+  runtime = "proton";
+  executable = "DemonLordJustABlock.exe";
+
+  gamescope = {
+    output-width = 1920;
+    output-height = 1080;
+    nested-width = 1920;
+    nested-height = 1080;
+    flags = {
+      "-r" = "60";
+      "--immediate-flips" = true;
+      "--expose-wayland" = true;
+    };
+  };
+
+  env = {
+    SteamAppId = "0";
+    SteamGameId = "0";
+    PROTON_NO_GAME_FIXES = "1";
+    DXVK_ASYNC = "1";
+    WINE_LARGE_ADDRESS_AWARE = "1";
+    LD_LIBRARY_PATH = "/usr/lib32:/usr/lib:/usr/lib64";
+  };
+
+  meta = {
+    description = "Demon Lord Just A Block (via Proton)";
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "demon-lord-just-a-block";
+  };
+}
