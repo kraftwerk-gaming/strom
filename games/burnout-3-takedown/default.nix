93f6582d55219723a432e42361e60e2727e98cb2
xXStromRiderXx <stromrider@fragged.zone>
2026-05-06 21:10:21 +0200

    burnout-3-takedown: pin source on the pin host, replace placeholder CID

--- games/burnout-3-takedown/default.nix
+++ games/burnout-3-takedown/default.nix
@@ -11,7 +11,7 @@ in
 mkPcsx2Game {
   name = "burnout-3-takedown";
   src = fetchIpfs {
-    cid = "placeholder";
+    cid = "QmTR7JR8zd4yENd9sudBy5iQBKRKKvYuB66vhymrRMLmSn";
     fallbackUrl = "https://archive.org/download/burnout-3-takedown-usa_202211/Burnout%203%20-%20Takedown%20%28USA%29.iso";
     hash = "sha256-re+KTNsyEM6c+ljytNKBdyWQYhdBsCNabQbm1fwvOQo=";
     name = "burnout-3-takedown-usa.iso";
