000c7e7da7cecf92034ad5eed3488c777fdacb75
xXStromRiderXx <stromrider@fragged.zone>
2026-05-07 22:22:48 +0200

    pcsx2: bind SDL-0 and SDL-1 controller indices
    SDL's controller enumeration can place a real gamepad at index 1 when
    motion sensors or virtual devices grab index 0. PCSX2 ORs duplicate
    bindings, so binding both indices means whichever slot the actual
    controller ends up on works.

--- lib/pcsx2.nix
+++ lib/pcsx2.nix
@@ -58,34 +58,38 @@ let
     [EmuCore/GS]
     OsdShowMessages = false
 
+    # Bind both SDL-0 and SDL-1 because SDL's controller enumeration can
+    # put a real gamepad at index 1 when motion sensors or virtual
+    # devices grab index 0. PCSX2 ORs the bindings, so whichever index
+    # the actual controller ends up on works.
     [Pad1]
-    Up = SDL-0/DPadUp
-    Right = SDL-0/DPadRight
-    Down = SDL-0/DPadDown
-    Left = SDL-0/DPadLeft
-    Triangle = SDL-0/FaceNorth
-    Circle = SDL-0/FaceEast
-    Cross = SDL-0/FaceSouth
-    Square = SDL-0/FaceWest
-    Select = SDL-0/Back
-    Start = SDL-0/Start
-    L1 = SDL-0/LeftShoulder
-    L2 = SDL-0/+LeftTrigger
-    R1 = SDL-0/RightShoulder
-    R2 = SDL-0/+RightTrigger
-    L3 = SDL-0/LeftStick
-    R3 = SDL-0/RightStick
-    Analog = SDL-0/Guide
-    LUp = SDL-0/-LeftY
-    LRight = SDL-0/+LeftX
-    LDown = SDL-0/+LeftY
-    LLeft = SDL-0/-LeftX
-    RUp = SDL-0/-RightY
-    RRight = SDL-0/+RightX
-    RDown = SDL-0/+RightY
-    RLeft = SDL-0/-RightX
-    LargeMotor = SDL-0/LargeMotor
-    SmallMotor = SDL-0/SmallMotor
+    Up = SDL-0/DPadUp & SDL-1/DPadUp
+    Right = SDL-0/DPadRight & SDL-1/DPadRight
+    Down = SDL-0/DPadDown & SDL-1/DPadDown
+    Left = SDL-0/DPadLeft & SDL-1/DPadLeft
+    Triangle = SDL-0/FaceNorth & SDL-1/FaceNorth
+    Circle = SDL-0/FaceEast & SDL-1/FaceEast
+    Cross = SDL-0/FaceSouth & SDL-1/FaceSouth
+    Square = SDL-0/FaceWest & SDL-1/FaceWest
+    Select = SDL-0/Back & SDL-1/Back
+    Start = SDL-0/Start & SDL-1/Start
+    L1 = SDL-0/LeftShoulder & SDL-1/LeftShoulder
+    L2 = SDL-0/+LeftTrigger & SDL-1/+LeftTrigger
+    R1 = SDL-0/RightShoulder & SDL-1/RightShoulder
+    R2 = SDL-0/+RightTrigger & SDL-1/+RightTrigger
+    L3 = SDL-0/LeftStick & SDL-1/LeftStick
+    R3 = SDL-0/RightStick & SDL-1/RightStick
+    Analog = SDL-0/Guide & SDL-1/Guide
+    LUp = SDL-0/-LeftY & SDL-1/-LeftY
+    LRight = SDL-0/+LeftX & SDL-1/+LeftX
+    LDown = SDL-0/+LeftY & SDL-1/+LeftY
+    LLeft = SDL-0/-LeftX & SDL-1/-LeftX
+    RUp = SDL-0/-RightY & SDL-1/-RightY
+    RRight = SDL-0/+RightX & SDL-1/+RightX
+    RDown = SDL-0/+RightY & SDL-1/+RightY
+    RLeft = SDL-0/-RightX & SDL-1/-RightX
+    LargeMotor = SDL-0/LargeMotor & SDL-1/LargeMotor
+    SmallMotor = SDL-0/SmallMotor & SDL-1/SmallMotor
 
     [EmuCore/Speedhacks]
     EECycleRate = 0
